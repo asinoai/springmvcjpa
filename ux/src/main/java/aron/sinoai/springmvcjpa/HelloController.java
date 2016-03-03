bc476da82b37ca45bf298bcf2956191c41f9ef94
asinoai <aron.sinoai@linkonomics.com>
2016-03-03 17:30:09 +0200

    Fixing log4j

--- ux/src/main/java/aron/sinoai/springmvcjpa/HelloController.java
+++ ux/src/main/java/aron/sinoai/springmvcjpa/HelloController.java
@@ -1,5 +1,8 @@
 package aron.sinoai.springmvcjpa;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HelloController {
+	private static final Logger LOGGER = LogManager.getLogger(HelloController.class);
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
+		LOGGER.info("lala");
+
+
 		model.addAttribute("message", "Hello world!");
 		return "hello";
 	}
