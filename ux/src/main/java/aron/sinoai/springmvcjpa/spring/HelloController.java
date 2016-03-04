09150d59b1e397b28316768944625ed219ea8af2
asinoai <aron.sinoai@linkonomics.com>
2016-03-04 16:34:58 +0200

    xml-less spring

--- ux/src/main/java/aron/sinoai/springmvcjpa/spring/HelloController.java
+++ ux/src/main/java/aron/sinoai/springmvcjpa/spring/HelloController.java
@@ -1,4 +1,4 @@
-package aron.sinoai.springmvcjpa;
+package aron.sinoai.springmvcjpa.spring;
 
 
 import org.apache.logging.log4j.LogManager;
@@ -9,14 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("testHello")
 public class HelloController {
 	private static final Logger LOGGER = LogManager.getLogger(HelloController.class);
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		LOGGER.info("lala");
-
 
 		model.addAttribute("message", "Hello world!");
 		return "hello";
---
+++ ux/src/main/java/aron/sinoai/springmvcjpa/spring/WebConfig.java
@@ -0,0 +1,38 @@
+package aron.sinoai.springmvcjpa.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+        registry.addResourceHandler("/xml/**").addResourceLocations("/xml/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+
+    @Bean
+    public InternalResourceViewResolver resolver() {
+        final InternalResourceViewResolver result = new InternalResourceViewResolver();
+        result.setPrefix("/WEB-INF/pages/");
+        result.setSuffix(".jsp");
+
+        return result;
+    }
+
+}
